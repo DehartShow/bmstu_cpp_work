571a0baaa71a5e2e34e4cdbca9858a715ebad4d4
DehartShow <voznyukdima02032005@gmail.com>
2023-11-14 19:06:35 +0300

    update let_6

--- let6_DB/let6.cpp
+++ let6_DB/let6.cpp
@@ -1,1 +1,59 @@
 #include "let6.h"
+
+int main(int argc, char **argv) {
+  std::fstream file(argv[1], std::ios::binary | std::ios::in);
+  if (!file) {
+    std::cout << "Ошибка открытия файла" << std::endl;
+    return 1;
+  }
+  u_int32_t count_students;
+  file.read(reinterpret_cast<char *>(&count_students), sizeof(count_students));
+
+  std::vector<Student> students;
+  students.resize(count_students);
+
+  std::string user_command;
+  user_command = argv[2];
+
+  for (size_t i = 0; i < count_students, file.peek() != EOF; ++i) {
+    Student student;
+    std::getline(file, student.name, '\0');
+    file.read(reinterpret_cast<char *>(&student.age), sizeof(int));
+    file.read(reinterpret_cast<char *>(&student.rating), sizeof(int));
+    students[i] = std::move(student);
+  }
+
+  if (user_command == "-a") {
+    std::sort(students.begin(), students.end(), compare_by_age);
+    std::vector<Student>::iterator beg = students.begin();
+    std::vector<Student>::iterator top = students.begin() + 15;
+    for (auto it = beg; beg != top; ++beg) {
+      std::cout << beg->name << std::endl;
+    }
+  } else if (user_command == "-o") {
+    std::sort(students.begin(), students.end(), compare_by_age);
+    std::sort(students.begin(), students.end(), compare_by_alphabet);
+    std::vector<Student>::iterator beg = students.begin();
+    std::vector<Student>::iterator top = students.begin() + 15;
+    for (auto it = beg; beg != top; ++beg) {
+      std::cout << beg->name << std::endl;
+    }
+  } else if (user_command == "-g") {
+    std::sort(students.begin(), students.end(), compare_by_rating);
+    std::vector<Student>::iterator beg = students.begin();
+    std::vector<Student>::iterator top = students.begin() + 15;
+    for (auto it = beg; beg != top; ++beg) {
+      std::cout << beg->name << std::endl;
+    }
+  } else if (user_command == "-r") {
+    std::sort(students.begin(), students.end(), compare_by_rating);
+    std::sort(students.begin(), students.end(), compare_by_alphabet);
+    std::vector<Student>::iterator beg = students.begin();
+    std::vector<Student>::iterator top = students.begin() + 15;
+    for (auto it = beg; beg != top; ++beg) {
+      std::cout << beg->name << std::endl;
+    }
+  } else {
+    std::cout << "Введена неверная команда" << std::endl;
+  }
+}
--- let6_DB/let6.h
+++ let6_DB/let6.h
@@ -14,7 +14,6 @@ struct Student {
   int age;
   int rating;
 };
-
 bool compare_by_rating(const Student &a, const Student &b) {
   return a.rating > b.rating;
 }
@@ -23,68 +22,6 @@ bool compare_by_age(const Student &a, const Student &b) {
 }
 bool compare_by_alphabet(const Student &a, const Student &b) {
   return a.name < b.name;
-}
-
-int main() {
-  std::string command;
-  std::fstream file(
-      "/home/dehart/BMSTU/cmake-example-repo/let6_DB/students.bin",
-      std::ios::binary | std::ios::in);
-  if (!file) {
-    std::cout << "Ошибка открытия файла" << std::endl;
-    return 1;
-  }
-  u_int32_t count_students;
-  file.read(reinterpret_cast<char *>(&count_students), sizeof(count_students));
-
-  std::vector<Student> students;
-  students.resize(count_students);
-
-  for (size_t i = 0; i < count_students, file.peek() != EOF; ++i) {
-    Student student;
-    std::getline(file, student.name, '\0');
-    file.read(reinterpret_cast<char *>(&student.age), sizeof(int));
-    file.read(reinterpret_cast<char *>(&student.rating), sizeof(int));
-    students[i] = std::move(student);
-  }
-
-  std::string user_command;
-  std::cout << "Введите команду:";
-  std::cin >> user_command;
-
-  if (user_command == "-a") {
-    std::sort(students.begin(), students.end(), compare_by_age);
-    std::vector<Student>::iterator beg = students.begin();
-    std::vector<Student>::iterator top = students.begin() + 15;
-    for (auto it = beg; beg != top; ++beg) {
-      std::cout << beg->name << std::endl;
-    }
-  } else if (user_command == "-o") {
-    std::sort(students.begin(), students.end(), compare_by_age);
-    std::sort(students.begin(), students.end(), compare_by_alphabet);
-    std::vector<Student>::iterator beg = students.begin();
-    std::vector<Student>::iterator top = students.begin() + 15;
-    for (auto it = beg; beg != top; ++beg) {
-      std::cout << beg->name << std::endl;
-    }
-  } else if (user_command == "-g") {
-    std::sort(students.begin(), students.end(), compare_by_rating);
-    std::vector<Student>::iterator beg = students.begin();
-    std::vector<Student>::iterator top = students.begin() + 15;
-    for (auto it = beg; beg != top; ++beg) {
-      std::cout << beg->name << std::endl;
-    }
-  } else if (user_command == "-r") {
-    std::sort(students.begin(), students.end(), compare_by_rating);
-    std::sort(students.begin(), students.end(), compare_by_alphabet);
-    std::vector<Student>::iterator beg = students.begin();
-    std::vector<Student>::iterator top = students.begin() + 15;
-    for (auto it = beg; beg != top; ++beg) {
-      std::cout << beg->name << std::endl;
-    }
-  } else {
-    std::cout << "Введена неверная команда" << std::endl;
-  }
 }
 
 #endif  // LET6_DB_LET6_H_
